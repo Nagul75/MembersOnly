cd60def299908e7f0844c625f87c5bd38abc1ca2
Nagul75 <nagulk697@gmail.com>
2025-03-09 10:53:08 +0530

    Add messages.created_at to query

--- db/queries.js
+++ db/queries.js
@@ -1,7 +1,7 @@
 const pool = require('./pool')
 
 async function getMessages() {
-    const {rows} = await pool.query("SELECT users.firstname, messages.title, messages.message FROM messages JOIN users ON messages.id = users.id ORDER BY messages.created_at DESC;")
+    const {rows} = await pool.query("SELECT users.firstname, messages.title, messages.message, messages.created_at FROM messages JOIN users ON messages.id = users.id ORDER BY messages.created_at DESC;")
     return rows
 }
 
